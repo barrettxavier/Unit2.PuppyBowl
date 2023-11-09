0d15788f12bb941fcd958e590b5a0cfbec76fd0e
barrettxavier <barrettxavier@yahoo.com>
2023-11-09 12:35:52 -0500

    added li to ul and added event listener

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const main = document.querySelector(`main`);
 
 const state = {
   puppyNames: [],
@@ -10,20 +11,43 @@ const getDataApiFunction = async () => {
     const response = await fetch(`${apiURL}/players`);
     const dataPull = await response.json();
     const playerData = dataPull.data;
-    // console.log(playerData.players);
+    console.log(playerData);
 
     // Update state with puppy names
     state.puppyNames = playerData.players.map(player => player.name);
 
     console.log(state.puppyNames);
-    return state.puppyNames;
+
+    createList();
   } catch (error) {
     console.error('Error fetching player data:', error);
   }
 };
 
-getDataApiFunction();
+const createList = () => {
+  const ul = document.createElement(`ul`);
+  state.puppyNames.forEach(name => {
+    const li = document.createElement(`li`);
+    li.innerText = name;
+    ul.appendChild(li);
 
+    li.addEventListener(`click`, (event) => {
+      console.log(event.target.innerText);
+
+      const html = `
+      <h1> Hello </h1>
+      `
+      main.innerHTML= html;
+    })
+  });
+  main.appendChild(ul);
+};
+
+// const clickableLink = () => {
+//   const clickPlayer = 
+// }
+
+getDataApiFunction();
 
 
 
